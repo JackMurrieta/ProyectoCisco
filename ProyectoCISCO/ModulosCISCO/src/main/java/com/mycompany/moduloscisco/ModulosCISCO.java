039e40f86dc2679b25432787dc01a0bb3990542d
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2025-03-30 14:59:42 -0700

    ESQUELETO PROYECTO

---
+++ ProyectoCISCO/ModulosCISCO/src/main/java/com/mycompany/moduloscisco/ModulosCISCO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.moduloscisco;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ModulosCISCO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ProyectoCISCO/PresentacionCISCO/src/main/java/com/mycompany/presentacioncisco/PresentacionCISCO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.presentacioncisco;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PresentacionCISCO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
